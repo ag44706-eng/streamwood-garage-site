e4a8bf0ecade7ca309103dd30debbf0370d69d58
ag44706-eng <ag44706@gmail.com>
2026-05-28 12:51:01 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,3 @@
-export const metadata = {
-  title: "Streamwood Garage Tune-Up",
-  description: "Garage door tune-ups and maintenance",
-}
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
